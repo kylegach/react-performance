6417a8e723b500fd4d677497e8df8e4460720c94
kg666261 <kyle.gach@broadcom.com>
2021-08-02 15:16:27 -0600

    Complete 07.extra-1

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -2,11 +2,13 @@
 // http://localhost:3000/isolated/exercise/07.js
 
 import * as React from 'react'
+import {unstable_trace as trace} from 'scheduler/tracing'
 import reportProfile from '../report-profile'
 
 function Counter() {
   const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
+  const increment = () =>
+    trace('click', performance.now(), () => setCount(c => c + 1))
   return <button onClick={increment}>{count}</button>
 }
 
